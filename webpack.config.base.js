1f6dfb4ef57e12cd5ce60b572bf49c0c1dc5d7ec
Will <2598451201@qq.com>
2021-02-04 11:03:32 +0800

    update

---
+++ webpack.config.base.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "My App",
+      template: "src/assets/index.html",
+    }),
+  ],
+};
